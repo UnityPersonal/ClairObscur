89bc9f1dc82418cfd7de624bcbd61cf9b036e537
agagtmdtlr <briankunkel9@gmail.com>
2025-06-13 16:12:08 +0900

    스킬 에셋 데이터 추가

--- Assets/1.Scripts/Scriptable/SkillData.cs
+++ Assets/1.Scripts/Scriptable/SkillData.cs
@@ -5,6 +5,9 @@ using UnityEngine.Timeline;
 public class SkillData : ScriptableObject
 {
     public string skillName;
+
+    public int damage; // 콤보 공격시 각 피격당 데미지 계산 필요
+    
     // 스킬 배우는 비용
     public string LearnCost;
     // 스킬 사용 비용
